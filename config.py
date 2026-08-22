b7ff08e3ca76d61046b0c4b876917edc9051aa61
spirosvoic97-rgb <spirosvoic97@gmail.com>
2026-08-22 13:59:07 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 import streamlit as st
 import gspread
+import json
+import base64
 from google.oauth2.service_account import Credentials
 
 ICON_URL = "https://cdn-icons-png.flaticon.com/512/2953/2953361.png"
@@ -30,37 +32,35 @@ EXPENSE_CATEGORIES = [
 @st.cache_resource
 def get_sheets_connection():
     try:
-        # Δημιουργία credentials από τα Secrets
-        if "gcp_service_account" in st.secrets:
+        scopes = [
+            "https://www.googleapis.com/auth/spreadsheets",
+            "https://www.googleapis.com/auth/drive"
+        ]
+        
+        # 1. Έλεγχος για connections.gsheets με private_key_base64
+        if "connections" in st.secrets and "gsheets" in st.secrets["connections"]:
+            gs_sec = st.secrets["connections"]["gsheets"]
+            if "private_key_base64" in gs_sec:
+                # Αποκωδικοποίηση του Base64 JSON
+                decoded_bytes = base64.b64decode(gs_sec["private_key_base64"])
+                creds_info = json.loads(decoded_bytes.decode("utf-8"))
+                credentials = Credentials.from_service_account_info(creds_info, scopes=scopes)
+            else:
+                creds_dict = dict(gs_sec)
+                if "private_key" in creds_dict:
+                    creds_dict["private_key"] = creds_dict["private_key"].replace("\\n", "\n")
+                credentials = Credentials.from_service_account_info(creds_dict, scopes=scopes)
+                
+        # 2. Fallback σε gcp_service_account
+        elif "gcp_service_account" in st.secrets:
             creds_dict = dict(st.secrets["gcp_service_account"])
             if "private_key" in creds_dict:
                 creds_dict["private_key"] = creds_dict["private_key"].replace("\\n", "\n")
-            scopes = [
-                "https://www.googleapis.com/auth/spreadsheets",
-                "https://www.googleapis.com/auth/drive"
-            ]
             credentials = Credentials.from_service_account_info(creds_dict, scopes=scopes)
-            client = gspread.authorize(credentials)
-            
-        elif "connections" in st.secrets and "gsheets" in st.secrets["connections"]:
-            # Fallback για το παλιό format των secrets
-            gs_sec = dict(st.secrets["connections"]["gsheets"])
-            scopes = [
-                "https://www.googleapis.com/auth/spreadsheets",
-                "https://www.googleapis.com/auth/drive"
-            ]
-            
-            # Αν υπάρχει private_key_base64 ή απλό private_key
-            if "private_key" in gs_sec:
-                gs_sec["private_key"] = gs_sec["private_key"].replace("\\n", "\n")
-                credentials = Credentials.from_service_account_info(gs_sec, scopes=scopes)
-            else:
-                client = gspread.service_account_from_dict(gs_sec)
-                credentials = client.auth
-
-            client = gspread.authorize(credentials)
         else:
-            raise Exception("Δεν βρέθηκαν τα Google Credentials στα Streamlit Secrets.")
+            raise Exception("Δεν βρέθηκαν έγκυρα Google Credentials στα Secrets.")
+
+        client = gspread.authorize(credentials)
 
         # Σύνδεση με το Google Sheet
         spreadsheet = client.open("Personal Finance Tracker Data")
